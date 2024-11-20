cdec5e7c99948d955a9465f09c837a27ac9f4693
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-11-20 12:15:23 +0100

    feat: routes added

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JournalController;
 
-Route::get('/', [JournalController::class, 'index'])->name('home');
+Route::get('/journals', [JournalController::class, 'index'])->name('apihome'); 
+Route::delete('/journals/{id}', [JournalController::class, 'destroy'])->name('apidestroy'); 
+Route::post('/journals', [JournalController::class, 'store'])->name('apistore'); 
+Route::put('/journals/{id}', [JournalController::class, 'update'])->name('apiupdate');
