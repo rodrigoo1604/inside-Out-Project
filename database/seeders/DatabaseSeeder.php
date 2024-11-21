0fec38aef922471eab371f9c2d1a0ef50d730b8f
sr-calcetines <pgavilansanc@gmail.com>
2024-11-21 14:24:20 +0100

    fix: fixed factory and img names

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,9 +21,54 @@ class DatabaseSeeder extends Seeder
         //    'email' => 'test@example.com',
         //]);
         
-        Journal::factory(10)->create([
-            'entry' => 'Hoy es mi cum !!!',
-            'emotion' => 'img/emotions/alegria.webp',
+        Journal::factory()->create([
+            'entry' => 'Its my birthday !!!',
+            'emotion' => 'img/emotions/joy.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I lost the bus',
+            'emotion' => 'img/emotions/sadness.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I forgot to bring my lunch',
+            'emotion' => 'img/emotions/anger.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'The project is due tomorrow',
+            'emotion' => 'img/emotions/anxiety.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I have nothing to do',
+            'emotion' => 'img/emotions/boredom.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I have broccoli for lunch',
+            'emotion' => 'img/emotions/disgust.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I peed my pants',
+            'emotion' => 'img/emotions/embarrasment.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I would like that hairstyle',
+            'emotion' => 'img/emotions/envy.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'Why is that clown on my bedroom?',
+            'emotion' => 'img/emotions/fear.webp',
+        ]);
+
+        Journal::factory()->create([
+            'entry' => 'I miss my grandma',
+            'emotion' => 'img/emotions/nostalgia.webp',
         ]);
 
         //Journal::factory(10)->create();
